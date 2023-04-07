1fec58516d9825eed4b4ba8729e75bc5153a5fd1
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2023-04-07 12:55:18 +0300

    Added an implementation for symbol event counter

--- src/main/java/main/SymbolEventCounter.java
+++ src/main/java/main/SymbolEventCounter.java
@@ -1,7 +1,6 @@
 package main;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.java.io.TextInputFormat;
@@ -10,7 +9,6 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
 import org.apache.flink.core.fs.Path;
-import org.apache.flink.metrics.Counter;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -18,21 +16,16 @@ import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.source.FileProcessingMode;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.util.Collector;
 
-// Generate files with -n number of words using the following command
-//(sed -e "/^[A-Z]/d" -e "/'s\$/d" | shuf -n 50000000 | fmt -w 200) </usr/share/dict/words > file.txt
-//yes this is a replicated input | head -c 1000000KB > file.txt
-//head -c 100MB /dev/urandom > file.txt
-////home/skalogerakis/Documents/Workspace/FlinkCheckpoint/Data/DataFiles/
-// FINAL head -c 600MB <(strings </dev/urandom) > file.txt
 
 // To get metrics via the rest api, use curl from the terminal
 // http://localhost:8081/jobs/455e17c95ffba35504f06224de661b76/metrics?get=restartingTime,downtime
+
+// TODO Isolate only the first column from the rest -> cat debs2022-gc-trading-day-08-11-21.csv | cut -d, -f1 > test.csv
 // TODO Important -> head -c 50M input.csv > output.csv  -> Isolate 50Mb from an initial file
 
-public class WordCountFiles {
+public class SymbolEventCounter {
 
     public static void main(String[] args) throws Exception {
 
@@ -71,45 +64,35 @@ public class WordCountFiles {
 
         DataStreamSource<String> input = env.readFile(inputFormat, filePath, FileProcessingMode.PROCESS_CONTINUOUSLY, 60000l);
 
-        DataStream<String> words = input
-//                .map(new RichMapFunction<String, String>() {
-//                    private transient Counter counter;
-//                    @Override
-//                    public void open(Configuration config) {
-//                        this.counter = getRuntimeContext().getMetricGroup().counter("myCounter");
-//                    }
-//                    @Override
-//                    public String map(String value) throws Exception {
-//                        this.counter.inc();
-//                        return value;
-//                    }
-//                })
-                .flatMap(new FlatMapFunction<String, String>() {
-                    public void flatMap(String s,
-                                        Collector<String> collector) throws Exception {
+        DataStream<String> words = input.filter((x) -> !x.startsWith("#") && !x.startsWith("ID,SecType") && !x.isEmpty() )
+                                        .flatMap(new FlatMapFunction<String, String>() {
+                                            public void flatMap(String s,
+                                                                Collector<String> col) throws Exception {
 
-                        String[] split = s.split(" ");
-                        for (String s1 : split) {
-                            collector.collect(s1);
-                        }
-                    }
-                })
-                .name("Splitter")
-                .uid("Splitter");
+                                                String[] split = s.split(",", -1);
+                                                col.collect(split[0]);
+                                            }
+                                        })
+                                        .name("Splitter")
+                                        .uid("Splitter");
 
         DataStream<Tuple2<String, Integer>> wordCount = words.keyBy((s) -> s)
                                                              .process(new StatefulReduceFunc())
-                                                             .name("Word Counter")
-                                                             .uid("Word Counter");
+                                                             .name("Symbol Counter")
+                                                             .uid("Symbol Counter");
 
         wordCount.print();
 
-        env.execute("Wordcount");
+        env.execute("SymbolCounter");
     }
 
     private static class StatefulReduceFunc extends KeyedProcessFunction<String, String, Tuple2<String, Integer>> {
         private transient ValueState<Integer> count;
 
+        public void open(Configuration parameters) {
+            ValueStateDescriptor<Integer> valueStateDescriptor = new ValueStateDescriptor<Integer>("count", Integer.class);
+            count = getRuntimeContext().getState(valueStateDescriptor);
+        }
         public void processElement(String s,
                                    Context context,
                                    Collector<Tuple2<String, Integer>> collector) throws Exception {
@@ -118,9 +101,6 @@ public class WordCountFiles {
             collector.collect(new Tuple2<>(s, currentCnt));
         }
 
-        public void open(Configuration parameters) {
-            ValueStateDescriptor<Integer> valueStateDescriptor = new ValueStateDescriptor<Integer>("count", Integer.class);
-            count = getRuntimeContext().getState(valueStateDescriptor);
-        }
+
     }
 }
