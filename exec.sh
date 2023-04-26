9668e66481e2f5c8540e995166e50bad77b0e123
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2023-04-26 18:02:26 +0300

    Minor change

--- exec.sh
+++ exec.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-PATH_FILE=$1
-FULL_PATH="/media/localdisk/skalogerakis/recovery/${PATH_FILE}"
+# For Symbol Event Counter
+#PATH_FILE=$1
+#FULL_PATH="/media/localdisk/skalogerakis/recovery/${PATH_FILE}"
+#
+#if [ -z "$1" ]; then
+#    echo "No argument supplied"
+#else
+#  echo "Execute application for ${PATH_FILE}"
+#  /media/localdisk/skalogerakis/flink/bin/flink run -d /media/localdisk/skalogerakis/recovery/FlinkCheckpoint/target/FlinkCheckpoint-1.0-SNAPSHOT.jar -c hdfs:///flink-checkpoints -file ${FULL_PATH}
+#fi
+
+
+SIZE=$1
+#FULL_PATH="/media/localdisk/skalogerakis/recovery/${PATH_FILE}"
 
 if [ -z "$1" ]; then
     echo "No argument supplied"
 else
-  echo "Execute application for ${PATH_FILE}"
-  /media/localdisk/skalogerakis/flink/bin/flink run -d /media/localdisk/skalogerakis/recovery/FlinkCheckpoint/target/FlinkCheckpoint-1.0-SNAPSHOT.jar -c hdfs:///flink-checkpoints -file ${FULL_PATH}
+  echo "Execute Synthetic Source Generator for size ${SIZE} MB"
+  /media/localdisk/skalogerakis/flink/bin/flink run -d /media/localdisk/skalogerakis/recovery/FlinkCheckpoint/target/FlinkCheckpoint-1.0-SNAPSHOT.jar -c hdfs:///flink-checkpoints -size ${SIZE}
 fi
-
