a3bd5b38d7e7e15dfe048980d8f5cfc081e00745
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2023-04-28 11:37:08 +0300

    Added clearing caches in the execution script

--- exec.sh
+++ exec.sh
@@ -18,6 +18,9 @@ SIZE=$1
 if [ -z "$1" ]; then
     echo "No argument supplied"
 else
+  sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches "
+  sleep 1
+
   echo "Execute Synthetic Source Generator for size ${SIZE} MB"
   /media/localdisk/skalogerakis/flink/bin/flink run -d /media/localdisk/skalogerakis/recovery/FlinkCheckpoint/target/FlinkCheckpoint-1.0-SNAPSHOT.jar -c hdfs:///flink-checkpoints -size ${SIZE}
 fi
